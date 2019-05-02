19e62a7f4c53bc60b903e6611a3b8362bca722ba
Harnek Sidhu <harnek.sidhu@traderev.com>
2019-05-02 11:25:01 -0400

    Upload base scaffolding of eks terraform

--- backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "traderev-tf-state-store"
-    region         = "us-east-1"
-    encrypt        = "true"
-    dynamodb_table = "traderev-tf-locks"
-    key            = "tf-tr-eks.tfstate"
-  }
-}
